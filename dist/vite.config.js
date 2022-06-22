70fd9bb413de32d475ecca2cd121e5dd303cf92b
Sky XYZ <jay@wild.xyz>
2022-06-22 11:12:41 -0700

    Updates

--- dist/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/three.js-droptest/',
-  plugins: [react()]
-})
